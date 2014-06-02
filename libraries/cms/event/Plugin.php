c84c6e6219e33f9aa63bdf3b91be0a76b866d606
wilsonge <georgejameswilson@googlemail.com>
2014-06-02 04:42:35 +0100

    Make setting plugin result easy

--- libraries/cms/event/Plugin.php
+++ libraries/cms/event/Plugin.php
@@ -21,15 +21,56 @@ defined('JPATH_PLATFORM') or die;
 class Plugin extends Event
 {
 	/**
+	 * The result of triggering the plugin event
+	 *
+	 * @var    boolean
+	 * @since  3.4
+	 */
+	protected $result = true;
+
+	/**
 	 * Forbid stopping the event propagation.
 	 *
 	 * @return  void
 	 *
-	 * @since   1.0
+	 * @since   3.4
 	 * @throws  \BadMethodCallException
 	 */
 	public function stop()
 	{
 		throw new \BadMethodCallException('You are not allowed to stop a plugin event');
+	}
+
+	/**
+	 * Allows someone to set a result. Each result is merged together to give a final
+	 * overall return as to the success of all the listeners.
+	 *
+	 * @param  boolean  $result  The result to register
+	 *
+	 * @return  void
+	 *
+	 * @since   3.4
+	 * @throws  \InvalidArgumentException
+	 */
+	public function setResult($result)
+	{
+		if (!is_bool($result))
+		{
+			throw new \InvalidArgumentException('The result must be a boolean');
+		}
+
+		$this->result = ($result && $this->result);
+	}
+
+	/**
+	 * Allows someone to get the result.
+	 *
+	 * @return  boolean  The result
+	 *
+	 * @since   3.4
+	 */
+	public function getResult()
+	{
+		return $this->result;
 	}
 }
