54faf1b3a45e0e75561adad374ad1de43d32ac1c
wilsonge <georgejameswilson@googlemail.com>
2014-06-08 15:45:38 +0100

    Add json view

--- libraries/cms/view/cmsjson.php
+++ libraries/cms/view/cmsjson.php
@@ -1,7 +1,30 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: George
- * Date: 08/06/14
- * Time: 15:43
- */ 
+ * @package     Joomla.Cms
+ * @subpackage  View
+ *
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+/**
+ * Prototype JView class.
+ *
+ * @package     Joomla.Libraries
+ * @subpackage  View
+ * @since       3.4
+ */
+class JViewCmsjson extends JViewCms
+{
+	/**
+	 * Method to render the view.
+	 *
+	 * @return  string  The rendered view.
+	 *
+	 * @since   3.2
+	 */
+	public function render()
+	{
+		return json_encode($this->getData());
+	}
+}
