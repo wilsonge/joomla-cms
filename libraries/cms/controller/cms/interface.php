760ae40c247006c49c8113ad9c8738c38fc3ca4f
zero-24 <zero-24@users.noreply.github.com>
2014-08-22 18:11:15 +0200

    no direct access

--- libraries/cms/controller/cms/interface.php
+++ libraries/cms/controller/cms/interface.php
@@ -7,6 +7,8 @@
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
+defined('JPATH_PLATFORM') or die;
+
 /**
  * Interface for a CMS controller needed for dispatching
  *
