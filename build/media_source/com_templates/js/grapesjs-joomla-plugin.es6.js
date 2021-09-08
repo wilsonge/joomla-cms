0f1438e8b79e4a21f5548287ac5ffb9dcc84520d
wilsonge <georgejameswilson@googlemail.com>
2021-09-08 04:00:14 +0100

    More stuff to plugins

--- build/media_source/com_templates/js/grapesjs-joomla-plugin.es6.js
+++ build/media_source/com_templates/js/grapesjs-joomla-plugin.es6.js
@@ -1,16 +1,51 @@
-Joomla = window.Joomla || {};
+/* global grapesjs */
+((grapesjs, Joomla) => {
+  grapesjs.plugins.add('joomla-plugin', (editor, options) => {
+    editor.setStyle({
+      selectors: ['j-class'],
+      style: {
+        width: '100%',
+        background: 'red',
+        padding: '10px',
+        margin: '20px',
+      },
+    });
 
-grapesjs.plugins.add('joomla-plugin', (editor, options) => {
-  editor.BlockManager.add('joomla-component', {
-    label: options.componentLabel || Joomla.Text._('COM_TEMPLATES_PAGEBUILDER_BLOCK_JOOMLA_COMPONENT'),
-    category: options.componentCategory || Joomla.Text._('COM_TEMPLATES_PAGEBUILDER_JOOMLA_CATEGORY'),
-    content: {
-      style: { padding: '10px' },
-      content: '<jdoc:include type="component" />',
-    },
-    attributes: {
-      class: 'fas fa-swatchbook',
-    },
-    droppable: false,
+    /** We use a custom model here so that we can override the rendering in the grapesjs editor */
+    editor.DomComponents.addType('jcomponent', {
+      isComponent: (el) => el.tagName === 'JDOC:INCLUDE' && el.attributes.type.value === 'component',
+
+      // Model definition
+      model: {
+        // Default properties
+        defaults: {
+          tagName: 'jdoc:include',
+          droppable: false,
+          copyable: false,
+          void: true,
+          attributes: {
+            type: 'component',
+          },
+        },
+      },
+      view: {
+        tagName: 'div',
+        onRender({ el }) {
+          // Adds a class for our styling which we've added above
+          el.classList.add('j-class');
+        },
+      },
+    });
+
+    editor.BlockManager.add('joomla-component', {
+      label: options.componentLabel || Joomla.Text._('COM_TEMPLATES_PAGEBUILDER_BLOCK_JOOMLA_COMPONENT'),
+      category: options.componentCategory || Joomla.Text._('COM_TEMPLATES_PAGEBUILDER_JOOMLA_CATEGORY'),
+      content: {
+        type: 'jcomponent',
+      },
+      attributes: {
+        class: 'fas fa-swatchbook',
+      },
+    });
   });
-});
+})(grapesjs, Joomla);
