11f1568ed7d7be75693ff1c226a1053aebd5d481
wilsonge <georgejameswilson@googlemail.com>
2021-09-08 15:59:51 +0100

    TODO etc

--- administrator/components/com_templates/src/View/Template/HtmlView.php
+++ administrator/components/com_templates/src/View/Template/HtmlView.php
@@ -179,6 +179,7 @@ class HtmlView extends BaseHtmlView
 			$this->form->setFieldAttribute('source', 'syntax', $ext);
 			$this->source = $this->get('Source');
 			$this->type   = 'file';
+			// TODO: Cheap and nasty for now! Probably based on a template (XML?) property claiming support!
 			$this->isPageBuilderLayout = ($this->form->getField('filename')->value === '/test.php');
 		}
 		elseif (in_array($ext, $imageTypes))
